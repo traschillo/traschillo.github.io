06f7104d61fd39a6423b50064ee9811b385fa024
traschillo <31389722+traschillo@users.noreply.github.com>
2018-04-04 09:52:25 -0400

    Addicting Game
    Pong Game Project

--- creative-computing/connectfour/pong.js
+++ creative-computing/connectfour/pong.js
@@ -44,4 +44,34 @@ function mousePressed() {
 
     }
     } 
+
+  document.getElementById('timer').innerHTML =
+  5 + ":" + 00;
+startTimer();
+
+function startTimer() {
+  var presentTime = document.getElementById('timer').innerHTML;
+  var timeArray = presentTime.split(/[:]+/);
+  var m = timeArray[0];
+  var s = checkSecond((timeArray[1] - 1));
+  if(s==59){m=m-1}
+  
+  document.getElementById('timer').innerHTML =
+    m + ":" + s;
+
+    if(m==0 && s==0){
+
+        window.location.assign("pongcrash.html")
+        //add link to new crash page
+
+    }
+
+  setTimeout(startTimer, 1000);
+}
+
+function checkSecond(sec) {
+  if (sec < 10 && sec >= 0) {sec = "0" + sec};
+  if (sec < 0) {sec = "59"};
+  return sec;
+}
         
